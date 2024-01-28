b7fd7d1d4385af5bcd82df6488713743ef7874be
Ho Anh Minh <yamchahuyvs@gmail.com>
2024-01-28 12:48:43 +0700

    [new thorn]

--- Assets/Script/Thorn.cs
+++ Assets/Script/Thorn.cs
@@ -23,6 +23,7 @@ public class Thorn : MonoBehaviour
             PlayerController playerController = collision.GetComponent<PlayerController>();
             playerController.canMove = false;
             playerController.time = 3f;
+            photonView.RPC("SetActiveObstacle_RPC", RpcTarget.All, false);
             SetActiveObstacle_RPC(false);
         }
         if (collision.CompareTag("Player"))
