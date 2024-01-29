7dc5d9f5963c270cc1e1964a673061fcd21939fa
Le Tuan Dung <dung2002ss@gmail.com>
2024-01-29 07:50:24 +0700

    fixed shuriken animation

---
+++ Assets/Script/Player/PlayerCollide.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollide : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Chỉnh tag của object trong Unity
+        //Darts là tag của skill 
+        //Phi tiêu trong map chưa có tag -> Tạo tag mới hoặc add tag darts vào prefabs Thorn
+        if (collision.CompareTag("Darts"))
+        {
+
+        }
+    }
+}
---
+++ Assets/Script/Skill/Rotate360.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Rotate360 : MonoBehaviour
+{
+    public float rotationSpeed = 5f;
+
+    void Update()
+    {
+        RotateAroundZ();
+    }
+
+    void RotateAroundZ()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
